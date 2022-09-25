a3ded6d0d145ba2eb377457102412475d466bf1c
Siddhant Tandon <76514713+siddhantt24@users.noreply.github.com>
2022-09-25 23:59:37 +0530

    Update MainActivity.kt

--- app/src/main/java/com/priyank/drdelivery/MainActivity.kt
+++ app/src/main/java/com/priyank/drdelivery/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
         }
         super.onCreate(savedInstanceState)
 
-        setContent {
+       //Breaking The App
             DrDeliveryTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
