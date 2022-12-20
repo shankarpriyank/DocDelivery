b500a8b5fc19e68d57b466bf1f2c3116799ad978
Shivam Verma <100941430+Photon3009@users.noreply.github.com>
2022-12-20 16:55:47 +0530

    Update ExtractLinkFromString.kt

--- app/src/main/java/com/priyank/drdelivery/shipmentDetails/domain/ExtractLinkFromString.kt
+++ app/src/main/java/com/priyank/drdelivery/shipmentDetails/domain/ExtractLinkFromString.kt
@@ -7,9 +7,10 @@ class ExtractLinkFromString {
     // Also refactor the code to make so that new pattern can be added easily in future...
 
     fun findLink(email: String): Triple<Boolean, String, String> {
+        val link = ""
         val pattern =
             Regex("http:\\/\\/delivery\\..+?\\.flipkart\\.com\\/([A-Za-z0-9\\?\\=&\\/\\\\\\+]+)")
-        val link = ""
+        
         val isLinkpresent = pattern.containsMatchIn(email)
         return if (isLinkpresent) {
             Triple(true, pattern.find(email)!!.value, "Flipkart")
